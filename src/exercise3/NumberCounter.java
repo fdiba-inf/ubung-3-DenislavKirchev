84a5a0f6eb1b252e2aec048dd8ca651e4dd4eb78
DenislavKirchev <denislav_delon@abv.bg>
2020-10-14 18:31:43 +0000

    Übung 3

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -7,12 +7,12 @@ public class NumberCounter{
   public static void main (String[] args){
 
     Scanner input = new Scanner(System.in);
-    int sum = 0;
+    double sum = 0;
     double average = 0;
     int positiveCounter=0;
     int negativeCounter = 0;
     int counter = 0;
-    System.out.println("Enter number: ");
+    
     int num = input.nextInt();
     
     while(num != 0){
