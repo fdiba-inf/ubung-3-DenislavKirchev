b40736c54e01bbebd3cb6f77da4af690b13cb64a
DenislavKirchev <denislav_delon@abv.bg>
2020-10-14 18:37:58 +0000

    Übung 3

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -7,14 +7,15 @@ public class NumberSearch {
     public static void main(String[] args) {
 
       int counter=0;
-      for(int i=100;i<=1000;i++){
+      for(int i=100; i<=1000; i++){
         if(i % 30 == 0){
-          System.out.print(i + " ");
+          
           counter++;
           if(counter == 10){
             counter=0;
             System.out.println();
           }
+          System.out.print(i + " ");
         }
       }
     }
